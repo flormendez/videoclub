3792b1d049bf57c4cd9347b4185b542717ad3d41
Florencia <florenciamendez93@gmail.com>
2019-04-10 17:22:46 -0300

    search by name, order by columns, pagination

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -2,57 +2,64 @@ const controller = {};
 
 controller.list = (req, res) => {
   req.getConnection((err, conn) => {
-    conn.query('SELECT * FROM movies', (err, movies) => {
-     if (err) {
-      res.json(err);
-     }
-     res.render('movies', {
+    conn.query("SELECT * FROM movies", (err, movies) => {
+      if (err) {
+        res.json(err);
+      }
+      res.render("movies", {
         data: movies
-     });
+      });
     });
   });
 };
 
 controller.save = (req, res) => {
   const data = req.body;
-  console.log(req.body)
+  console.log(req.body);
   req.getConnection((err, connection) => {
-    const query = connection.query('INSERT INTO movies set ?', data, (err, customer) => {
-      console.log(customer)
-      res.redirect('/');
-    })
-  })
+    const query = connection.query(
+      "INSERT INTO movies set ?",
+      data,
+      (err, movie) => {
+        console.log(movie);
+        res.redirect("/");
+      }
+    );
+  });
 };
 
 controller.edit = (req, res) => {
   const { id } = req.params;
   req.getConnection((err, conn) => {
     conn.query("SELECT * FROM movies WHERE id = ?", [id], (err, rows) => {
-      res.render('movies_edit', {
+      res.render("movies_edit", {
         data: rows[0]
-      })
+      });
     });
   });
 };
 
 controller.update = (req, res) => {
   const { id } = req.params;
-  const newCustomer = req.body;
+  const newMovie = req.body;
   req.getConnection((err, conn) => {
-
-  conn.query('UPDATE movies set ? where id = ?', [newCustomer, id], (err, rows) => {
-    res.redirect('/');
-  });
+    conn.query(
+      "UPDATE movies set ? where id = ?",
+      [newMovie, id],
+      (err, rows) => {
+        res.redirect("/");
+      }
+    );
   });
 };
 
 controller.delete = (req, res) => {
   const { id } = req.params;
   req.getConnection((err, connection) => {
-    connection.query('DELETE FROM movies WHERE id = ?', [id], (err, rows) => {
-      res.redirect('/');
+    connection.query("DELETE FROM movies WHERE id = ?", [id], (err, rows) => {
+      res.redirect("/");
     });
   });
-}
+};
 
 module.exports = controller;
